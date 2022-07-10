a750b61751ad7d6e873633ba29dabf986776c33d
chiku3 <p314chiku.y@gmail.com>
2022-07-10 06:41:44 +0000

    [UPDATE]views

---
+++ db/migrate/20220710064041_add_commomt_to_items.rb
@@ -0,0 +1,5 @@
+class AddCommomtToItems < ActiveRecord::Migration[5.2]
+  def change
+    add_column :items, :comment, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_10_054642) do
+ActiveRecord::Schema.define(version: 2022_07_10_064041) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_07_10_054642) do
     t.string "image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "comment"
   end
 
   create_table "posts", force: :cascade do |t|
