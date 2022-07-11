0a629aff65033bd8eff2282eb76b7ebef7a71da2
chiku3 <p314chiku.y@gmail.com>
2022-07-11 06:13:50 +0000

    [UPDATE]show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,12 @@ class PostsController < ApplicationController
 
   end
 
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to post_path()
+  end
+  
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   root to: 'homes#top'
-  
-  resources :posts, only:[:show, :create, :destroy]
-  
+
+  resources :posts, only:[:show, :create, :update, :destroy]
+
   resources :items, only:[:show, :index]
-  
+
 end
